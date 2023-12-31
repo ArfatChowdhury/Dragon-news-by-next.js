2bfd928d9ba148a51e0c13a7d91108b6549e2b56
Arfat Chowdhury <arfatahsan60@gmail.com>
2024-01-01 01:27:13 +0600

    create footer

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,11 +1,75 @@
-import { Box, Container } from "@mui/material";
-
+import { Box, Button, Container, IconButton } from "@mui/material";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import Link from "next/link";
 
 const Footer = () => {
+    const NavItems = [
+        {
+            route: 'Home',
+            pathname: "/",
+        },
+        {
+            route: 'Pages',
+            pathname: "/pages",
+        },
+        {
+            route: 'Category',
+            pathname: "/category",
+        },
+        {
+            route: 'News',
+            pathname: "/news",
+        },
+        {
+            route: 'Post',
+            pathname: "/post",
+        },
+        {
+            route: 'Contact',
+            pathname: "/contact",
+        },
+
+    ];
     return (
         <Box className='bg-black py-10'>
             <Container>
+                <Box
+                    className='w-full text-center'
+                    sx={{
+                        "& svg": {
+                            color: "white",
 
+                            fontSize: '35px'
+                        }
+                    }}>
+                    <IconButton>
+                        <FacebookIcon></FacebookIcon>
+                    </IconButton>
+                    <IconButton>
+                        <FacebookIcon></FacebookIcon>
+                    </IconButton>
+                    <IconButton>
+                        <FacebookIcon></FacebookIcon>
+                    </IconButton>
+                    <IconButton>
+                        <FacebookIcon></FacebookIcon>
+                    </IconButton>
+                    <IconButton>
+                        <FacebookIcon></FacebookIcon>
+                    </IconButton>
+                </Box>
+
+                <Box className='w-full text-center' >
+                    {NavItems.map((item) => (
+                        <Link key={item} href={item.pathname}>
+                            <Button className='text-white'>
+                                {item.route}
+                            </Button>
+                        </Link>
+
+                    ))}
+                </Box>
+                <h1 className="text-gray-300 w-full text-center text-sm">@2023 Dragon News. Design by Programming Hero developed by Arfat Chowdhury</h1> 
             </Container>
         </Box>
     );
