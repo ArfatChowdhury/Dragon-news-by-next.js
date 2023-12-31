10f25222ecef8aa443fd8a0a817a7332fd4ed1e3
Arfat Chowdhury <arfatahsan60@gmail.com>
2024-01-01 01:18:52 +0600

    create NAvbar

---
+++ src/components/shared/Footer.js
@@ -0,0 +1,14 @@
+import { Box, Container } from "@mui/material";
+
+
+const Footer = () => {
+    return (
+        <Box className='bg-black py-10'>
+            <Container>
+
+            </Container>
+        </Box>
+    );
+};
+
+export default Footer;
--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -13,151 +13,92 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
+import Image from 'next/image';
+import logo from '@/assets/logo.png'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import Link from 'next/link';
+import { Stack } from '@mui/material';
 
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
-function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+const NavItems = [
+    {
+        route: 'Home',
+        pathname: "/",
+    },
+    {
+        route: 'Pages',
+        pathname: "/pages",
+    },
+    {
+        route: 'Category',
+        pathname: "/category",
+    },
+    {
+        route: 'News',
+        pathname: "/news",
+    },
+    {
+        route: 'Post',
+        pathname: "/post",
+    },
+    {
+        route: 'Contact',
+        pathname: "/contact",
+    },
+   
+];
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+function Navbar() {
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
 
-  return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
+    return (
+        <AppBar position="static" className='bg-black'>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+                    <Image src={logo} alt='logo'></Image>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+                    <Box className='w-full text-center' >
+                        {NavItems.map((item) => (
+                            <Link key={item} href={item.pathname}>
+                                <Button className='text-white'>
+                                    {item.route}
+                                </Button>
+                            </Link>
+
+                        ))}
+                    </Box>
+
+                    <Box>
+
+                       <Stack direction='row' sx={{
+                        "& svg": {
+                            color:"white",
+                        fontSize: '30px'
+                        }
+                       }}>
+                       <IconButton>
+                            <FacebookIcon></FacebookIcon>
+                        </IconButton>
+                        <IconButton>
+                            <FacebookIcon></FacebookIcon>
+                        </IconButton>
+                        <IconButton>
+                            <FacebookIcon></FacebookIcon>
+                        </IconButton>
+                        <IconButton>
+                            <FacebookIcon></FacebookIcon>
+                        </IconButton>
+                        <IconButton>
+                            <FacebookIcon></FacebookIcon>
+                        </IconButton>
+                       </Stack>
+                    </Box>
+
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 }
-export default ResponsiveAppBar;
+export default Navbar;
